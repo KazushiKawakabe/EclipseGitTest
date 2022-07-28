af4a66b795d85bcc56b2501fbd0d7f47eda1cf29
kazushi.kawakabe <kazushi.kawakabe@exceed-net.co.jp>
2022-07-28 15:43:34 +0900

    bashのテスト

--- GitTest/src/test/Test_one.java
+++ GitTest/src/test/Test_one.java
@@ -5,5 +5,6 @@ public class Test_one {
 	public static void main(String[] args) {
 		System.out.println("Gitのマスターに上げます");
 		System.out.println("testブランチに上げます");
+		System.out.println("bashのテストをします");
 	}
 }
